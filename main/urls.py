2f8b82ecb2a8c1cd20393508498300d0a4a273e3
Aleksandr <greenteamer@bk.ru>
2013-12-30 07:30:28 +0700

    сайт доделан 1 версия

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
 from main.views import *
+from portfolio.views import *
 
 
 urlpatterns = patterns('',
@@ -8,5 +9,6 @@ urlpatterns = patterns('',
     url(r'^sozdanie-saitov/$', SaitView.as_view(), name='sait'),
     url(r'^mobilnie-prilojeniya/$', MobileView.as_view(), name='mobile'),
     url(r'^kontakty/$', 'feedback.views.contact'),
+    url(r'^portfolio/$', FolioView.as_view(), name='folio'),
     url(r'^prodvijenie/$', ProdvijenieView.as_view(), name='prodvijenie'),
 )
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,1 +1,18 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+from django.views.generic import TemplateView, DetailView
+from blog.models import Post
+from review.models import *
+from portfolio.models import *
+
+class FolioView(TemplateView):
+    template_name = 'folio.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(FolioView, self).get_context_data()
+        context['posts'] = Post.objects.all()
+        context['reviews'] = Review.objects.all()
+        context['portfolios'] = Portfolio.objects.filter(main_choice='main')
+        # context['slides'] = Slider.objects.all()
+        # context['games'] = Game.objects.all()
+        # context['supernumeraries'] = Supernumerary.objects.all()
+        return context
