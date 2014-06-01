5e41bf5bd744789aacec974d72550f51c9594c5b
greenteamer <greenteamer@bk.ru>
2014-06-02 03:40:35 +0700

    defaults

--- forward/urls.py
+++ forward/urls.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
